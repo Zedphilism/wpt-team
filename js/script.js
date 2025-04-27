00d240eba93a08cbf4e454c4b6cf779f0c43bd78
Zedphilism <zaidi3116@gmail.com>
2025-04-28 01:11:33 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Random background image changer
 document.addEventListener('DOMContentLoaded', function() {
   const backgrounds = [
-    'images/background1.jpg',
-    'images/background2.jpg'
+    'images/background1.webp',
+    'images/background2.webp'
   ];
 
   const randomImage = backgrounds[Math.floor(Math.random() * backgrounds.length)];
